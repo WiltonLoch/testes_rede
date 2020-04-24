e0779f7e557be49cef3f72f78a5a9958663053b4
wiltonloch <wilton.loch@gmail.com>
2020-04-24 10:19:07 -0300

    adição de experimentos

--- experimentos.py
+++ experimentos.py
@@ -6,6 +6,19 @@ from mininet.link import TCLink
 from mininet.clean import Cleanup
 from topologia import arvoreMultiNos
 from testes import Testes
+from datetime import timedelta
+import time
+
+def rajadas_sl(rede, carga, execucoes = 1, intervalo = 0):
+    resultados = []
+    for i in range(1, execucoes + 1):
+        if intervalo:
+            time.sleep(intervalo)
+        print("Executando teste %s/%s" % (i, execucoes), end = "\r")
+        intermediario = Testes.emitir_sl(rede, carga, 0, 10)
+        print(intermediario)
+        resultados.append(intermediario)
+    return resultados
 
 def experimento():
     Cleanup.cleanup()
@@ -13,10 +26,49 @@ def experimento():
     rede = Mininet(topo = topo, host = CPULimitedHost, link = TCLink)
     rede.start()
     dumpNodeConnections(rede.hosts)
-    print("Preparando o ambiente de testes ", end = '')
-    #for i in range(0, 9):
-    Testes.emitir_sl(rede, carga = '1K', 0, 10)    
-    Testes.iperfMultiNos(rede, carga = '250M', comportamento = 'bisection');
+
+    print("\n===============================")
+    print("\nPreparando o ambiente de testes\n")
+    print("===============================\n")
+
+    for i in range(0, 10):
+        print("h%s <-> h%s -- " % (i, i + 10), end = "")
+        print(Testes.emitir_sl(rede, '1K', i, i + 10))
+
+    print("\n==============================")
+    print("\nLendo arquivos de configuração\n")
+    print("==============================\n")
+
+    entrada = open("config", "r")
+    if entrada.mode != 'r':
+        print("Não foi possível abrir o arquivo de configuração")
+        return
+    parametros = [x.split()[2:] for x in entrada.read().splitlines()]
+    execucoes = int(parametros[0][0])
+    intervalo = float(parametros[1][0])
+    cargas_sl = parametros[2]
+    cargas_trafego = parametros[3]
+    del parametros
+
+    print("\n===================")
+    print("\nDisparando SLs base\n")
+    print("===================\n")
+    for i in cargas_sl:
+        resultados = rajadas_sl(rede, i, execucoes, intervalo)
+        media = sum(resultados, timedelta(0))/len(resultados)
+        print("\n", media)
+    print("\n===========================")
+    print("\nIniciando adição de tráfego\n")
+    print("===========================\n")
+    for i in cargas_trafego:
+        print("Trafego adicional: " + i)
+        for j in cargas_sl:
+            print("Carga de teste: " + j)
+            Testes.iperfMultiNos(rede, i, 'bisection', tempo = execucoes*intervalo + 3)
+            resultados = rajadas_sl(rede, i, execucoes, intervalo)
+            media = sum(resultados, timedelta(0))/len(resultados)
+            print("\n", media)
+            time.sleep(5)
     rede.stop()
 
 def main():
--- testes.py
+++ testes.py
@@ -5,6 +5,7 @@ from mininet.node import CPULimitedHost
 from mininet.link import TCLink
 from mininet.clean import Cleanup
 from topologia import arvoreMultiNos
+from datetime import timedelta
 import time
 
 class Testes:
@@ -12,32 +13,38 @@ class Testes:
     def dividirCarga(carga, qtd_hosts):
         if carga[-1] == "G":
             carga = str(int(carga.strip('G'))*1024) + 'M'
-        print(carga)
         carga_numerica = int(carga.strip("KM"))
         carga_numerica = carga_numerica//(qtd_hosts//2);
         return str(carga_numerica) + carga[-1]
 
     @classmethod
-    def iperfMultiNos(cls, rede, carga = '', comportamento = ''):
+    def iperfMultiNos(cls, rede, carga, comportamento, tempo = 10):
         hosts = rede.hosts
         qtd_hosts = len(hosts)
+        flags = '--time %s' % tempo
         if comportamento == 'bisection':
             if carga:
                 carga = '--bitrate ' + cls.dividirCarga(carga, qtd_hosts)
-            print(carga)
             for i in range(qtd_hosts//2):
                 hosts[(i + qtd_hosts//2)%qtd_hosts].cmd('iperf3 --server > /tmp/h%s_rcv.out &' % ((i + qtd_hosts//2)%qtd_hosts))
-                hosts[i].cmd('iperf3 --time 10 ' + carga + ' --client %s --bidir > /tmp/%s_send.out &' % (hosts[(i + qtd_hosts//2)%qtd_hosts].IP(), hosts[i]))
+                hosts[i].cmd('iperf3 ' + carga + ' --client %s --bidir ' % hosts[(i + qtd_hosts//2)%qtd_hosts].IP() + flags + ' > /tmp/%s_send.out &' % hosts[i])
 
     @staticmethod
     def carregarCPU(rede, carga = 0):
         for h in rede.hosts:
             h.cmd("stress-ng -t 8 -c 1 -l %s &" % carga)
 
+
     @staticmethod
-    def emitir_sl(rede, carga = '', origem, destino):
+    def emitir_sl(rede, carga, origem, destino):
             rede.hosts[destino].cmd('(socat -u TCP-LISTEN:5440 stdout | wc -c) >> /tmp/sl_bytes.out &')
-            print(rede.hosts[origem].cmd('tail -c ' + carga + ' data | socat -u stdin TCP-CONNECT:%s:5440' % rede.hosts[11].IP()))
+            tempos = rede.hosts[origem].cmd('sleep 0.5 && tail -c ' + carga + ' data | socat -lu -ddd -u stdin TCP-CONNECT:%s:5440' % rede.hosts[destino].IP()).rsplit('socket', 1)[0].split('reading', 1)[1].splitlines()
+            tempo_inicial, tempo_final = tempos[1], tempos[-1]
+            tempo_inicial = [float(x) for x in tempo_inicial.split()[1].split(':')]
+            tempo_final = [float(x) for x in tempo_final.split()[1].split(':')]
+            tempo_inicial = timedelta(hours = tempo_inicial[0], minutes = tempo_inicial[1], seconds = tempo_inicial[2])
+            tempo_final = timedelta(hours = tempo_final[0], minutes = tempo_final[1], seconds = tempo_final[2])
+            return tempo_final - tempo_inicial
 
 #if __name__ == '__main__':
 #    main()
