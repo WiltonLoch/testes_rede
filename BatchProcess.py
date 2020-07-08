4001f9df5bcb7e8d41657ed8d1fa371b0e8353fb
wiltonloch <wilton.loch@gmail.com>
2020-07-08 17:08:22 -0300

    adição do ljf

--- BatchProcess.py
+++ BatchProcess.py
@@ -15,6 +15,20 @@ class BatchProcessing:
             multiplier *= 1000 * 1000
         return int(carga[:-1]) * multiplier
 
+    @staticmethod
+    def largestLoad(carga):
+        multiplier = 1000
+        if(carga[-1] == 'M'):
+            multiplier *= 1000
+        elif(carga[-1] == 'G'):
+            multiplier *= 1000 * 1000
+        return 1/(int(carga[:-1]) * multiplier)
+
+
     @classmethod
     def sjf(cls, cargas):
         cargas.sort(key = cls.smallestLoad)
+
+    @classmethod
+    def ljf(cls, cargas):
+        cargas.sort(key = cls.largestLoad)
